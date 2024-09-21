eaa764031509fd3d33617ccecf9d44fbbb65b288
Túlio <tulio.sdg@gmail.com>
2024-09-21 19:57:01 -0300

    :sparkles: feat: páginas das perguntas

---
+++ pages/pergunta_2.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from components import *
+
+st.set_page_config(page_title="Python",
+                   layout="wide",
+                   page_icon="imgs/bazico.png")
+
+menu_navegacao()
+
+st.write("Pergunta 2")
---
+++ pages/pergunta_6.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from components import *
+
+st.set_page_config(page_title="Campanha em Salvador",
+                   layout="wide",
+                   page_icon="imgs/bazico.png")
+
+menu_navegacao()
+
+st.write("Pergunta 6")
