329696022795223b151433452e154bd8444ecc78
Túlio <tulio.sdg@gmail.com>
2024-09-21 20:55:07 -0300

    :pencil: texto de apresentação

--- main.py
+++ main.py
@@ -14,6 +14,19 @@ st.divider()
 st.write("""
 E ai, pessoal da Bázico! Tudo tranquilo?
 
-Estou muito feliz por ter passado de fase no processo e chegado até o desafio de dados.
+Fiquei muito feliz por ter passado de fase no processo e chegado até o desafio de dados :) 
          
+Ao ver que a apresentação era por minha conta, pensei em trazer algo que fosse diferente.
+Então aqui eu vos apresento a minha
+
+### Resposta ao Desafio de Dados ≠
+         
+#### Introdução
+> Esse é um **Data App interativo**, construído usando o Streamlit, uma biblioteca de Python para criação de aplicações web. Nele você pode navegar pelas perguntas e respostas do desafio, além de visualizar gráficos e tabelas com os dados que utilizei para responder as perguntas.
+
+#### Navegando pelas Perguntas
+> Para navegar pelas perguntas, basta clicar no menu lateral esquerdo (caso não esteja aparecendo, no canto superior esquerdo terá uma setinha, basta clicar). Lá você encontrará as perguntas do desafio e poderá visualizar as respostas que preparei para cada uma delas.  
+
+
+Qualquer dúvida, estou à disposição para esclarecer. Espero que gostem!
 """)
