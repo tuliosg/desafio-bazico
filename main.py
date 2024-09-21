6a568edbd6d34c897df4767f879c8e92cec600b4
Túlio <tulio.sdg@gmail.com>
2024-09-21 19:52:39 -0300

    🎉 init: criando o repo do desafio da bázico

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from components import *
+
+st.set_page_config(page_title="Desafio Bázico",
+                   layout="wide",
+                   page_icon="imgs/bazico.png")
+
+menu_navegacao()
+
+st.title("Desafio Analítico e Estratégico ≠")
+st.subheader("Desafio de Dados")
+st.divider()
+
+st.write("""
+E ai, pessoal da Bázico! Tudo tranquilo?
+
+Estou muito feliz por ter passado de fase no processo e chegado até o desafio de dados.
+         
+""")
