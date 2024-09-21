64e270d9bd0fdffc67f68a9d9dd39704528b50e0
Túlio <tulio.sdg@gmail.com>
2024-09-21 19:55:17 -0300

    :sparkles: feat: menu de navegação

---
+++ components.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def menu_navegacao():
+    st.sidebar.image("imgs/bazico_svg.svg", use_column_width="auto")
+    st.sidebar.divider()
+    st.sidebar.markdown("### Página inicial")
+    st.sidebar.page_link(page='main.py', label='Desafio Bázico **≠**')
+    st.sidebar.markdown("### Perguntas")
+    st.sidebar.page_link(page="pages/pergunta_1.py", label="1. SQL")
+    st.sidebar.page_link(page="pages/pergunta_2.py", label="2. Python")
+    st.sidebar.page_link(page="pages/pergunta_3.py", label="3. Ferramentas de Análise")
+    st.sidebar.page_link(page="pages/pergunta_4.py", label="4. Problema de Negócio")
+    st.sidebar.page_link(page="pages/pergunta_5.py", label="5. Transformação de Dados")
+    st.sidebar.page_link(page="pages/pergunta_6.py", label="6. Campanha em Salvador")
