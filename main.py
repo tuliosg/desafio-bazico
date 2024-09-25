09e90bcb5286ceab19b5bb9d4c7231758301d2ca
Túlio <tulio.sdg@gmail.com>
2024-09-24 22:55:29 -0300

    :sparkles: feat: resposta da última pergunta

--- main.py
+++ main.py
@@ -23,11 +23,16 @@ Então aqui eu vos apresento a minha:
 ### Resposta ao Desafio de Dados ≠
          
 #### Introdução
-> Esse é um **Data App interativo**, construído usando o Streamlit, uma biblioteca de Python para criação de aplicações web. Nele você pode navegar pelas perguntas e respostas do desafio, além de visualizar gráficos e tabelas com os dados que utilizei para responder as perguntas.
+> Esse é um **Data App interativo**, construído usando o Streamlit, uma biblioteca de Python para criação de aplicações web. Nele você pode navegar pelas perguntas e respostas do desafio, além de visualizar todos os códigos e dados que utilizei*.
 
 #### Navegando pelas Perguntas
-> Para navegar pelas perguntas, basta clicar no menu lateral esquerdo (caso não esteja aparecendo, no canto superior esquerdo terá uma setinha, basta clicar). Lá você encontrará as perguntas do desafio e poderá visualizar as respostas que preparei para cada uma delas.  
+> Para acessar as perguntas, basta clicar no menu lateral esquerdo (caso não esteja aparecendo, no canto superior esquerdo terá uma setinha, basta clicar). Lá você encontrará as perguntas do desafio e poderá visualizar as respostas que preparei para cada uma delas.  
 
 
 Qualquer dúvida, estou à disposição para esclarecer. Espero que gostem!
+
+---    
+
+> **O desenvolvimento desse Data App foi registrado no GitHub e todos os códigos estão disponíveis no repositório [desafio-bazico](https://github.com/tuliosg/desafio-bazico)*
+         
 """)
--- pages/pergunta_6.py
+++ pages/pergunta_6.py
@@ -16,5 +16,18 @@ números você utilizou para estimar isso? Não existe certo ou errado, o que
 importa é como você faria essa estimativa.
 
 ---
+
+### Resposta
+
+Comecei essa estimativa dando uma boa olhada no Instagram de vocês. As informações que mais me chamaram atenção foram as seguintes:
+- O time da Bázico chegou em Salvador no dia 03/09 mas só começou as operações no dia 04;
+- Pelo instagram, a campanha durou algo entre 20 e 23 dias;
+- Nos reels em que apareciam algumas pessoas vestindo as roupas da bázico, cheguei em uma estimativa aproximada de 25 peças por reel;
+- Houve também o sorteio de 40 camisas, aumentando o total;
+- No dia 23/09 houve um post dizendo que haviam mais de 500 bázicas em Salvador.
+
+Levando em consideração essas informações, fiz algumas contas para chegar em um mínimo e um máximo:
+- **Mínimo**: 20 dias de campanha x 25 peças por reel + 40 do sorteio = **540 peças**
+- **Máximo**: 23 dias de campanha x 25 peças por reel + 40 do sorteio = **615 peças**
 """
 )
